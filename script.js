51e866105ced3fd210fc3b0250ed15b891248af0
Arshdeep1247 <arshdeepsingh08@algomau.ca>
2025-10-31 23:55:03 +0530

    feat: show current background color value below the button

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const button = document.getElementById('colorBtn');
 button.addEventListener('click',() => {
     const randomColor = 'hsl(${Math.random() *360} , 70% , 80%)';
     document.body.style.backgroundColor = randomColor;
+    colorValue.textContent = 'Current Color: ${randomColor}';
 })
