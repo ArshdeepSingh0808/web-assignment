b3f68b00f2bb12887f01dbad16ed59b19b152da8
Arshdeep1247 <arshdeepsingh08@algomau.ca>
2025-11-01 00:03:05 +0530

    feat: add reset button to restore default background color

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const button = document.getElementById('colorBtn');
+const colorValue = document.getElementById('colorValue');
+const resetBtn = document.getElementById('resetBtn');
+const defaultColor = '#f4f4f4';
 
 button.addEventListener('click',() => {
     const randomColor = 'hsl(${Math.random() *360} , 70% , 80%)';
     document.body.style.backgroundColor = randomColor;
     colorValue.textContent = 'Current Color: ${randomColor}';
+})
+
+resetBtn.addEventListener('click',() => {
+    document.body.style.backgroundColor = defaultColor;
+    colorValue.textContent = 'Current Color: ${defaultColor}';
 })
